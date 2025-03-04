7aef3745a2703e461f0097653970613d364b4ae0
giakhanh22024558 <127011768+giakhanh22024558@users.noreply.github.com>
2025-03-05 02:01:48 +0700

    add shimmer and shaking effect

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -108,6 +108,44 @@
             animation: floatIn 0.75s ease-out forwards;
         }
 
+        @keyframes moveUp {
+            0% {
+                transform: translateY(0);
+            }
+
+            50% {
+                transform: translateY(-10px);
+            }
+
+            100% {
+                transform: translateY(0);
+            }
+        }
+
+        @keyframes shake {
+
+            0%,
+            100% {
+                transform: translateX(0);
+            }
+
+            25% {
+                transform: translateX(-5px);
+            }
+
+            50% {
+                transform: translateX(5px);
+            }
+
+            75% {
+                transform: translateX(-5px);
+            }
+        }
+
+        .group-shake:hover .hover-move-up-shake {
+            animation: moveUp 0.3s ease-in-out, shake 0.5s ease-in-out 0.3s;
+        }
+        
     </style>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
