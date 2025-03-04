6c0a02cda0a9741c441db85024b98b9f9a98c600
giakhanh22024558 <127011768+giakhanh22024558@users.noreply.github.com>
2025-03-05 01:44:20 +0700

    add ranking table, link to ranking page

--- resources/views/student/contest/rank.blade.php
+++ resources/views/student/contest/rank.blade.php
@@ -58,5 +58,129 @@
             </div>
         </div>
 
+        <div class="container bg-white py-10 rounded-2xl mt-6">
+
+            <div class="text-center text-[#FFAA00] text-2xl uppercase font-bold">
+                Vinh danh bảng vàng
+            </div>
+
+            <div class="text-center text-2xl font-bold mt-3">
+                Cuộc thi "Tự Tin Vươn Mình Thế Giới"
+            </div>
+
+            <table class="table-auto w-full mt-5">
+                <thead class="shadow-[0px_10px_10px_-3px_rgba(0,0,0,0.15)]">
+                    <tr>
+                        <th class="text-center py-5">
+                            <div class="border-r-2 w-full">Hạng</div>
+                        </th>
+                        <th class="text-center">
+                            <div class="border-r-2 w-full">Avatar</div>
+                        </th>
+                        <th class="text-start">
+                            <div class="border-r-2 w-full pl-3">Họ và tên</div>
+                        </th>
+                        <th class="text-start">
+                            <div class="border-r-2 w-full pl-3">Điểm số</div>
+                        </th>
+                        <th class="text-start pl-3">Tỉnh thành phố </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="group hover:bg-[#F1FCF3] duration-300 hover:cursor-pointer border-y-2 border-dashed">
+                        <div>
+                            <td class="pt-5">
+                                <div class="flex w-full h-full justify-center shimmer items-center">
+                                    <img src="{{ asset('/assets/img/home/Group1.png') }}" alt="">
+                                </div>
+                            </td>
+                            <td class="relative pb-5">
+                                <div class="hidden group-hover:block duration-300 absolute -top-2 left-36 rounded-t-full rounded-r-full rounded-bl-none bg-green-200 border-2 border-green-600 text-[#21A642] px-3 text-nowrap font-medium">
+                                    Bạn cần <span>16</span> điểm để thăng hạng
+                                </div>  
+                                <div
+                                    class="shimmer relative bg-contain flex justify-center align-bottom pt-3">
+                                    <img src="{{ asset('/assets/img/home/Frame1.png') }}" alt="">
+                                    <img class="absolute self-end" src="{{ asset('/assets/img/home/Avatar.png') }}"
+                                        alt="">
+                                </div>
+                            </td>
+                            <td class="pl-4 pt-3 text-[#525266] font-medium">Lương Gia Khánh</td>
+                            <td class="pl-4 pt-3 font-bold group-hover:text-green-600">100</td>
+                            <td class="pl-4 pt-3 text-[#525266] font-medium">Thái Bình</td>
+                        </div>
+                    </tr>
+                    <tr class="border-y-2 border-dashed group hover:bg-[#F1FCF3] duration-300 hover:cursor-pointer">
+                        <div>
+                            <td class="pt-5">
+                                <div class="flex w-full h-full justify-center shimmer items-center">
+                                    <img src="{{ asset('/assets/img/home/Group2.png') }}" alt="">
+                                </div>
+                            </td>
+                            <td class="relative group pb-5">
+                                <div class="hidden group-hover:block duration-300 absolute -top-2 left-36 rounded-t-full rounded-r-full rounded-bl-none bg-green-200 border-2 border-green-600 text-[#21A642] px-3 text-nowrap font-medium">
+                                    Bạn cần <span>16</span> điểm để thăng hạng
+                                </div> 
+                                <div
+                                    class="shimmer relative bg-contain flex bg-no-repeat bg-center justify-center align-bottom pt-3">
+                                    <img src="{{ asset('/assets/img/home/Frame2.png') }}" alt="">
+                                    <img class="absolute self-end" src="{{ asset('/assets/img/home/Avatar.png') }}"
+                                        alt="">
+                                </div>
+                            </td>
+                            <td class="pl-4 pt-3 text-[#525266] font-medium">Lương Gia Khánh</td>
+                            <td class="pl-4 pt-3 font-bold group-hover:text-green-600">100</td>
+                            <td class="pl-4 pt-3 text-[#525266] font-medium">Thái Bình</td>
+                        </div>
+                    </tr>
+                    <tr class="border-y-2 border-dashed group hover:bg-[#F1FCF3] duration-300 hover:cursor-pointer">
+                        <div>
+                            <td class="pt-3">
+                                <div class="flex w-full h-full justify-center shimmer items-center">
+                                    <img src="{{ asset('/assets/img/home/Group3.png') }}" alt="">
+                                </div>
+                            </td>
+                            <td class="relative group pb-4">
+                                <div class="hidden group-hover:block duration-300 absolute -top-2 left-36 rounded-t-full rounded-r-full rounded-bl-none bg-green-200 border-2 border-green-600 text-[#21A642] px-3 text-nowrap font-medium">
+                                    Bạn cần <span>16</span> điểm để thăng hạng
+                                </div> 
+                                <div
+                                    class="shimmer bg-contain flex bg-no-repeat bg-center justify-center align-bottom pt-6">
+                                    <div class="border-4 border-amber-900 rounded-full">
+                                        <img src="{{ asset('/assets/img/home/Avatar.png') }}" alt="">
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="pl-4 pt-3 text-[#525266] font-medium">Lương Gia Khánh</td>
+                            <td class="pl-4 pt-3 font-bold group-hover:text-green-600">100</td>
+                            <td class="pl-4 pt-3 text-[#525266] font-medium">Thái Bình</td>
+                        </div>
+                    </tr>
+                    <tr class="group hover:bg-[#F1FCF3] duration-300 hover:cursor-pointer border-y-2 border-dashed">
+                        <div>
+                            <td class="pt-3 text-center text-xl font-semibold text-[#525266] group-hover:text-green-600">
+                                4
+                            </td>
+                            <td class="relative pb-5">
+                                <div class="hidden group-hover:block duration-300 absolute -top-2 left-36 rounded-t-full rounded-r-full rounded-bl-none bg-green-200 border-2 border-green-600 text-[#21A642] px-3 text-nowrap font-medium">
+                                    Bạn cần <span>16</span> điểm để thăng hạng
+                                </div>
+                                <div class="bg-contain flex bg-no-repeat bg-center justify-center align-bottom pt-6">
+                                    <div>
+                                        <img src="{{ asset('/assets/img/home/Avatar.png') }}" alt="">
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="pl-4 pt-3 text-[#525266] font-medium">Lương Gia Khánh</td>
+                            <td class="pl-4 pt-3 font-bold group-hover:text-green-600">100</td>
+                            <td class="pl-4 pt-3 text-[#525266] font-medium">Thái Bình</td>
+                        </div>
+                    </tr>
+                </tbody>
+            </table>
+
+        </div>
+
+
     </div>
 @endsection
